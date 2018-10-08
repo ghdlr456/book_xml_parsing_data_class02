7cb07f1a1eb97f2b0e2c17ef6b6c5ba6907b5414
Seo Sang won <33479927+zhaoSeo@users.noreply.github.com>
2018-10-08 14:20:32 +0900

    xml parsing

---
+++ File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  book_xml_parsing_data_class02
+//
+//  Created by Sang won Seo on 02/10/2018.
+//  Copyright © 2018 Sang won Seo. All rights reserved.
+//
+
+import Foundation
---
+++ book_xml_parsing_data_class02/BookData.swift
@@ -0,0 +1,14 @@
+//
+//  BookData.swift
+//  book_xml_parsing_data_class02
+//
+//  Created by Sang won Seo on 02/10/2018.
+//  Copyright © 2018 Sang won Seo. All rights reserved.
+//
+
+import Foundation
+class BookData {
+    var title = ""
+    var author = ""
+}
+
